56a76d5390265ddc317117dd22d250da738bea89
PMichelangelo <Pmichelangelo1377@gmail.com>
2024-06-01 01:57:41 +0300

    markup

---
+++ src/components/Features/Features.jsx
@@ -0,0 +1,21 @@
+import content from "./featuresContent"
+import styles from "./features.module.css"
+
+const Features = () => {
+  return (
+    <div>
+      <h1>Why {content.companyName} ?</h1>
+      <ul>
+        <li>{content.reputation}</li>
+        <li>{content.packages}</li>
+        <li>{content.expertise}</li>
+        <li>{content.services}</li>
+        <li>{content.businessesType}</li>
+        <li>{content.belief}</li>
+        <li>{content.benefits}</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Features
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import Hero from 'components/Hero/Hero';
+import Features from 'components/Features/Features';
 import Services from 'components/Services/Services';
 
 const HomePage = () => {
   return (
     <section>
       <Hero></Hero>
+      <Features/>
       <Services/>
     </section>
   );
