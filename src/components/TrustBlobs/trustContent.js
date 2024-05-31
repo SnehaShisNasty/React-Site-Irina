7e5e71706e469d7a69bc4bc1f816caaecd1a6702
PMichelangelo <Pmichelangelo1377@gmail.com>
2024-06-01 02:17:17 +0300

    markup

---
+++ src/components/TrustBlobs/trustContent.js
@@ -0,0 +1,8 @@
+export const items = [
+  { id: 1, text: 'You work directly with an accountant. Your case is handled by a specialist who has received qualifications and practical experience in Britain.' },
+  { id: 2, text: 'AUDIT CONSULTING is owner-managed, so you will work with one person throughout the entire collaboration.' },
+  { id: 3, text: 'The company is licensed and accredited. We are entitled to represent your interests before HMRC.' },
+  { id: 4, text: 'We handle your case from start to finish.' },
+  { id: 5, text: 'AUDIT CONSULTING  is regulated by many authorities in order to protect the interests of users.' },
+  { id: 6, text: 'We are from Ukraine, so we understand your peculiarities and values.'},
+]
