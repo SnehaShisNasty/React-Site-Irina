79610bcc2409039982e54d8826c2e55d7365ccdb
PMichelangelo <Pmichelangelo1377@gmail.com>
2024-06-03 01:10:22 +0300

    css first touch

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,26 +4,26 @@ import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       {/**********************Additional**********************/}
-      <div>
+      <div className={styles.column}>
         <h1>Additional</h1>
-        <ul>
+        <ul className={styles.list}>
           <li><a href="">Contact Us</a></li>
           <li><a href="">About Us</a></li>
           <li><a href="">Privacy Policy and Cookies</a></li>
         </ul>
-        <div>
+        <div className={styles.inputContainer}>
           <input
             type="email"
             placeholder='example@mail.com'/>
-          <button>JOIN US</button>
+          <button>Join us</button>
         </div>
       </div>
       {/**********************Services**********************/}
-      <div>
+      <div className={styles.column}>
         <h1>Services</h1>
-        <ul>
+        <ul className={styles.list}>
           <li><a href="">Business Consulting</a></li>
           <li><a href="">Company Formation Service</a></li>
           <li><a href="">Full Accounting And Tax Support</a></li>
@@ -32,14 +32,14 @@ const Footer = () => {
         </ul>
       </div>
       {/**********************Questions**********************/}
-      <div>
+      <div className={styles.column}>
         <h1>Questions</h1>
-        <ul>
+        <ul className={styles.list}>
           <li>{info.location}</li>
           <li>{info.tel}</li>
           <li>{info.email}</li>
         </ul>
-        <ul>
+        <ul className={styles.social}>
           <li><a href="">insta</a></li>
           <li><a href="">youtube</a></li>
           <li><a href="">facebook</a></li>
