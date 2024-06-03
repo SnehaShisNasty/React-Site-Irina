f6e89448454ab371dc7b4587cee91aec7d5e7eef
PMichelangelo <Pmichelangelo1377@gmail.com>
2024-06-04 01:57:20 +0300

    commit css fixes

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,6 +1,6 @@
 
 import { items } from '../../data/servies'
-//import styles from './services.module.css'
+import styles from './services.module.css'
 
 
 const Services = () => {
@@ -8,22 +8,26 @@ const Services = () => {
   const corporateServices = items.filter(item => item.type === 'corporate')
   const individualServices = items.filter(item => item.type === 'individual')
 
-  return(
-    <div>
-      <h1>Services</h1>
-      <p>Corporate services:</p>
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Services</h1>
+      <p className={styles.subTitle}>Corporate services:</p>
       <ul>
         {corporateServices.map(item => (
-          <li key={item.id}>
-            <a href="">{item.name} &#62;</a>
+          <li key={item.id} className={styles.item}>
+            <a href={item.address} className={styles.link}>
+              <span>{item.name}</span>
+            </a>
           </li>
         ))}
       </ul>
-      <p>Individual services:</p>
+      <p className={styles.subTitle}>Individual services:</p>
       <ul>
         {individualServices.map(item => (
-          <li key={item.id}>
-            <a href="">{item.name} &#62;</a>
+          <li key={item.id} className={styles.item}>
+            <a href={item.address} className={styles.link}>
+              <span>{item.name}</span>
+            </a>
           </li>
         ))}
       </ul>
--- src/data/contactInfo.js
+++ src/data/contactInfo.js
@@ -1,5 +1,5 @@
 const info = {
-  location: "Office 7 35-37 Ludgate Hill, London, England, EC4M 7JN",
+  location: "Office 7 35-37 Ludgate Hill, London, England",
   tel: "+44 755 312 1185",
   email: "info@faroscapital.co.uk"
 }
