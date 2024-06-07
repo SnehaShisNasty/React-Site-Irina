dafe99a4034d16849eb85fc02a9ccdb0115d4dc6
PMichelangelo <Pmichelangelo1377@gmail.com>
2024-06-08 01:13:50 +0300

    small-fixes

---
+++ src/img/brand/images.js
@@ -0,0 +1,15 @@
+import acca from './acca-logo.jpg'
+import att from './att-logo.jpg'
+import dext from './dext-logo.jpg'
+import sage from './sage-logo.jpg'
+import xero from './xero-logo.png'
+
+const images = [
+  { src: acca, alt: "acca-log" },
+  { src: att, alt: "att-log" },
+  { src: dext, alt: "dext-log" },
+  { src: sage, alt: "sage-log" },
+  { src: xero, alt: "xero-log" },
+];
+
+export default images
