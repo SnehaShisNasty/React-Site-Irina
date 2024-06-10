ecb9c4266b56b1df6c95454b474d20f9d60d98af
SnehaShisNasty <katirina18@gmail.com>
2024-06-10 15:32:06 +0100

    update node

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,85 +1,122 @@
-import info from "../../data/contactInfo"
-import sprite from "../../img/symbol-defs.svg"
-import styles from './footer.module.css'
+import info from '../../data/contactInfo';
+import sprite from '../../img/symbol-defs.svg';
+import styles from './footer.module.css';
 
 const Footer = () => {
-
-    const openMap = (address) => {
-    const mapUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)}`;
+  const openMap = address => {
+    const mapUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+      address
+    )}`;
     window.open(mapUrl, '_blank');
-    };
+  };
 
   return (
     <div className={styles.container}>
-      {/**********************Additional**********************/}
       <div className={styles.column}>
         <h1>Additional</h1>
         <ul className={styles.list}>
-          <li><a href="" className={styles.link}>Contact Us</a></li>
-          <li><a href="" className={styles.link}>About Us</a></li>
-          <li><a href="" className={styles.link}>Privacy Policy and Cookies</a></li>
+          <li>
+            <a href="#" className={styles.link}>
+              Contact Us
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.link}>
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.link}>
+              Privacy Policy and Cookies
+            </a>
+          </li>
         </ul>
         <form className={styles.inputContainer}>
           <input
             className={styles.inputForm}
             type="email"
-            placeholder='example@mail.com'/>
+            placeholder="example@mail.com"
+          />
           <button>Join us</button>
         </form>
       </div>
-      {/**********************Services**********************/}
       <div className={styles.column}>
         <h1>Services</h1>
         <ul className={styles.list}>
-          <li><a href="" className={styles.link}>Business Consulting</a></li>
-          <li><a href="" className={styles.link}>Company Formation Service</a></li>
-          <li><a href="" className={styles.link}>Full Accounting And Tax Support</a></li>
-          <li><a href="" className={styles.link}>Self-employment registration (UTR)</a></li>
-          <li><a href="" className={styles.link}>Self-Assessment Tax Return</a></li>
+          <li>
+            <a href="#" className={styles.link}>
+              Business Consulting
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.link}>
+              Company Formation Service
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.link}>
+              Full Accounting And Tax Support
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.link}>
+              Self-employment registration (UTR)
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.link}>
+              Self-Assessment Tax Return
+            </a>
+          </li>
         </ul>
       </div>
-      {/**********************Questions**********************/}
       <div className={styles.column}>
         <h1>Questions</h1>
         <ul className={styles.list}>
-          <li >
-            <a href=""
+          <li>
+            <a
+              href="#"
               className={styles.link}
-              onClick={() => openMap(info.location)}>
+              onClick={() => openMap(info.location)}
+            >
               {info.location}
             </a>
           </li>
           <li>
-            <a href={`tel:${info.tel}`} className={styles.link}>{info.tel}</a>
+            <a href={`tel:${info.tel}`} className={styles.link}>
+              {info.tel}
+            </a>
           </li>
           <li>
-            <a href={`mailto:${info.email}`} className={styles.link}>{info.email}</a>
+            <a href={`mailto:${info.email}`} className={styles.link}>
+              {info.email}
+            </a>
           </li>
         </ul>
         <ul className={styles.social}>
           <li>
-            <a href="" className={styles.linkSocial}>
+            <a href="#" className={styles.linkSocial}>
               <svg className={styles.iconSvg}>
-                <use href={`${sprite}#instagram`} className={styles.icon}/>
+                <use href={`${sprite}#instagram`} className={styles.icon} />
               </svg>
             </a>
           </li>
           <li>
-            <a href="" className={styles.linkSocial}>
+            <a href="#" className={styles.linkSocial}>
               <svg className={styles.iconSvg}>
                 <use href={`${sprite}#whatsapp`} className={styles.icon} />
               </svg>
             </a>
           </li>
           <li>
-            <a href="" className={styles.linkSocial}>
+            <a href="#" className={styles.linkSocial}>
               <svg className={styles.iconSvg}>
                 <use href={`${sprite}#youtube`} className={styles.icon} />
               </svg>
             </a>
           </li>
           <li>
-            <a href="" className={styles.linkSocial}>
+            <a href="#" className={styles.linkSocial}>
               <svg className={styles.iconSvg}>
                 <use href={`${sprite}#twitter`} className={styles.icon} />
               </svg>
@@ -88,7 +125,7 @@ const Footer = () => {
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
