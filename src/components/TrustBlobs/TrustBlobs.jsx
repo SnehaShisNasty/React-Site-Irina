be8fc0fde4ffcc7ec246c5ea3c8d7e59aa3e12f3
SnehaShisNasty <katirina18@gmail.com>
2024-06-10 18:43:28 +0100

    Update TrustBlobs.jsx

--- src/components/TrustBlobs/TrustBlobs.jsx
+++ src/components/TrustBlobs/TrustBlobs.jsx
@@ -1,28 +1,32 @@
-import { useRef } from 'react'
-import useVisibility from 'customeHooks/useVisivility'
-import { items } from './trustContent'
-import styles from "./trustBlobs.module.css"
+import { useRef } from 'react';
+import useVisibility from 'customeHooks/useVisivility';
+import { items } from './trustContent';
+import styles from './trustBlobs.module.css';
 
 const TrustBlobs = () => {
-
-  const containerRef = useRef(null)
-  const isVisible = useVisibility(containerRef)
+  const containerRef = useRef(null);
+  const isVisible = useVisibility(containerRef);
 
   return (
-    <div ref={containerRef} className={`${styles.container} ${isVisible ? styles.visible : ''}`}>
+    <div
+      ref={containerRef}
+      className={`${styles.container} ${isVisible ? styles.visible : ''}`}
+    >
       <h1 className={styles.title}>Why can you trust us ?</h1>
       <ul className={styles.list}>
         {items.map((item, index) => (
-          <li key={item.id} className={`${ styles.listItem } ${index === items.length - 1 ? styles.lastItem : ''}`}>
-            <p className={styles.text}>
-              {item.text}
-            </p>
+          <li
+            key={item.id}
+            className={`${styles.listItem} ${
+              index === items.length - 1 ? styles.lastItem : ''
+            }`}
+          >
+            <p className={styles.text}>{item.text}</p>
           </li>
         ))}
       </ul>
-      <button className={styles.btn}>Check why you can rely on us</button>
     </div>
-  )
-}
+  );
+};
 
-export default TrustBlobs
+export default TrustBlobs;
