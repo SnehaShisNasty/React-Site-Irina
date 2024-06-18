f7e616b4e885cd604bd877cb71e8a1799dd7685d
SnehaShisNasty <katirina18@gmail.com>
2024-06-18 07:41:12 +0100

    Update featuresContent.js

--- src/components/Features/featuresContent.js
+++ src/components/Features/featuresContent.js
@@ -1,12 +1,19 @@
 const content = {
-  companyName: "Audit Consalting",
-  reputation: "AUDIT CONSULTING has a reputation as one of the most reliable and effective accountancy companies.",
-  packages: "Our accountancy packages are ideal for contractors and freelancers, individual clients and businesses, large or small.",
-  expertise: "Our company expertly attends to all your taxation and business needs. Due to the expertise of our staff, our dedication and many years of experience in accounting, consulting and assessment, we can offer you the support that you can rely on.",
-  services: "We provide a wide range of accountancy services London and nationwide including personal self-assessment tax returns, annual accounts, payroll and bookkeeping to a large variety of different clients.",
-  businessesType: "We help a diverse range of businesses including start-ups, sole traders, limited companies, contractors, freelancers and landlords.",
-  belief: "We believe that in today's complex financial and business world, everyone needs reliable, professional accountancy advice and help in managing their personal and business finances.",
-  benefits: "Our highly regarded accounting includes a dedicated accountant, unlimited phone, email and chat support, annual accountants and much more.",
-}
+  companyName: 'AUDIT CONSULTING',
+  reputation:
+    'AUDIT CONSULTING has a reputation as one of the most reliable and effective accountancy companies.',
+  packages:
+    'Our accountancy packages are ideal for contractors and freelancers, individual clients and businesses, large or small.',
+  expertise:
+    'Our company expertly attends to all your taxation and business needs. Due to the expertise of our staff, our dedication and many years of experience in accounting, consulting and assessment, we can offer you the support that you can rely on.',
+  services:
+    'We provide a wide range of accountancy services London and nationwide including personal self-assessment tax returns, annual accounts, payroll and bookkeeping to a large variety of different clients.',
+  businessesType:
+    'We help a diverse range of businesses including start-ups, sole traders, limited companies, contractors, freelancers and landlords.',
+  belief:
+    "We believe that in today's complex financial and business world, everyone needs reliable, professional accountancy advice and help in managing their personal and business finances.",
+  benefits:
+    'Our highly regarded accounting includes a dedicated accountant, unlimited phone, email and chat support, annual accountants and much more.',
+};
 
-export default content
+export default content;
