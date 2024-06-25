1aae77cdcca594c265c8c2318b9757903e79505c
PMichelangelo <94895580+PMichelangelo@users.noreply.github.com>
2024-06-25 18:06:38 +0000

    Deploying to gh-pages from @ SnehaShisNasty/React-Site-Irina@3c59e1429d4b2313da75a3376621ce42eb22c281 🚀

---
+++ static/js/794.ec5d7944.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_site=self.webpackChunkreact_site||[]).push([[794],{1794:function(e,n,t){t.r(n);var s=t(1087),r=t(184);n.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h1",{children:"Cannot found this page"}),(0,r.jsx)(s.rU,{to:"/",children:"To home page"})]})}}}]);
+//# sourceMappingURL=794.ec5d7944.chunk.js.map
--- static/js/794.f294d738.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_site=self.webpackChunkreact_site||[]).push([[794],{794:function(e,n,t){t.r(n);var s=t(87),r=t(184);n.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h1",{children:"Cannot found this page"}),(0,r.jsx)(s.rU,{to:"/",children:"To home page"})]})}}}]);
-//# sourceMappingURL=794.f294d738.chunk.js.map
