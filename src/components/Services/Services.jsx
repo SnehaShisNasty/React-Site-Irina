84a5635f7034d17c03ba4398f59bec0e3f978cf5
SnehaShisNasty <katirina18@gmail.com>
2024-06-26 12:21:48 +0100

    fix

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,29 +2,34 @@ import { useRef } from 'react';
 import useVisibility from 'customeHooks/useVisivility';
 import { items } from '../../data/servies';
 import { useTranslation } from 'react-i18next';
-import sprite from '../../img/symbol-defs-services.svg'
+import sprite from '../../img/symbol-defs-services.svg';
 import styles from './services.module.css';
 
 const Services = () => {
   const containerRef = useRef(null);
   const isVisible = useVisibility(containerRef);
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   const corporateServices = items.filter(item => item.type === 'corporate');
   const individualServices = items.filter(item => item.type === 'individual');
 
-  const renderServiceCard = (item) => (
+  const renderServiceCard = item => (
     <div key={item.id} className={styles.card}>
-      <svg  className={styles.iconSvg}>
-        <use xlinkHref={`${sprite}#${item.icon}`} className={styles.icon}/>
+      <svg className={styles.iconSvg}>
+        <use xlinkHref={`${sprite}#${item.icon}`} className={styles.icon} />
       </svg>
       <h3 className={styles.cardTitle}>{t(`services.${item.name}`)}</h3>
-      <a href={item.address} className={styles.cardLink}>{t('services.learMore')}</a>
+      <a href={item.address} className={styles.cardLink}>
+        {t('services.learnMore')}
+      </a>
     </div>
   );
 
   return (
-    <div ref={containerRef} className={`${styles.container} ${isVisible ? styles.visible : ''}`}>
+    <div
+      ref={containerRef}
+      className={`${styles.container} ${isVisible ? styles.visible : ''}`}
+    >
       <h1 className={styles.title}>{t('services.title')}</h1>
       <p className={styles.subTitle}>{t('header.navMenu.corporateServices')}</p>
       <div className={styles.servicesFlex}>
@@ -33,7 +38,9 @@ const Services = () => {
       <div className={`${styles.servicesFlex} ${styles.secondRow}`}>
         {corporateServices.slice(3).map(renderServiceCard)}
       </div>
-      <p className={styles.subTitle}>{t('header.navMenu.individualServices')}</p>
+      <p className={styles.subTitle}>
+        {t('header.navMenu.individualServices')}
+      </p>
       <div className={styles.servicesFlex}>
         {individualServices.slice(0, 3).map(renderServiceCard)}
       </div>
@@ -42,6 +49,6 @@ const Services = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Services;
