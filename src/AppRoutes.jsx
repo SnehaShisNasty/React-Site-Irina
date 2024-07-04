64f4f891d2c76aced355f23a3829f02af7ac99d4
PMichelangelo <Pmichelangelo1377@gmail.com>
2024-07-05 01:29:10 +0300

    commit

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -4,7 +4,6 @@ import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from './components/SharedLayout/SharedLayout';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-const ServicesTypePage = lazy(() => import('./pages/ServicesTypePage/ServicesTypePage'))
 const ServicePage = lazy(()=> import('./pages/ServicePage/ServicePage'))
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
@@ -14,8 +13,6 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="about-us" />
-        <Route path="services/:type" element={<ServicesTypePage/>}/>
         <Route path='service/:serviceAddress' element={<ServicePage/>}/>
         <Route path="*" element={<NotFoundPage />} />
       </Route>
