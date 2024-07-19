13c4fe6477ddee1884d0413656390c450feb04ed
SnehaShisNasty <katirina18@gmail.com>
2024-07-19 20:06:00 +0100

    commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,9 +11,10 @@ const Hero = () => {
   const handleShow = () => setShowModal(true);
   const handleClose = () => setShowModal(false);
   return (
-    <div className={styles.overlay} id='homePage'>
+    <div className={styles.overlay} id="homePage">
       <div className={styles.hero}>
         <h1 className={styles.title}>{t('hero.title')}</h1>
+        <p className={styles.subtitle}>{t('hero.subtitle')}</p>
         <button onClick={handleShow} className={styles.btn}>
           {t('hero.getConsulting')}
         </button>
