897e6be4e78f6d1fdb25d40b217b87e8ac78d8e0
SnehaShisNasty <73851798+SnehaShisNasty@users.noreply.github.com>
2024-10-19 14:00:44 +0100

    Update telegram.js

--- src/api/telegram.js
+++ src/api/telegram.js
@@ -8,8 +8,8 @@ const telegramInstance = axios.create({
 
 export const sendForm = async (data, type) => {
   let message = `Aplication\n`;
-  message += `  Type: ${data.type} \n`;
-  message += `  Name: ${data.name} \n`;
+  message += `Type: ${data.type} \n`;
+  message += `Name: ${data.name} \n`;
   message += `Email: ${data.email} \n`;
   message += `Number: ${data.number} \n`;
   if (data.comment) {
